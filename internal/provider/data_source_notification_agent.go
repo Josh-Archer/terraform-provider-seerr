a5b8e69be497e8da20501e648fdb0b1d3b049d51
Josh Archer <jarcher32@gatech.edu>
2026-03-09 09:20:08 -0400

    fix: typed notification resources/data sources no longer deserialize plan/state through the old generic schema shape (#36)

--- internal/provider/data_source_notification_agent.go
+++ internal/provider/data_source_notification_agent.go
@@ -14,41 +14,6 @@ type NotificationClientDataSource struct {
 	agent  string
 }
 
-type NotificationClientDataSourceModel struct {
-	Enabled     types.Bool  `tfsdk:"enabled"`
-	EmbedPoster types.Bool  `tfsdk:"embed_poster"`
-	TypesMask   types.Int64 `tfsdk:"types"`
-
-	Discord               *NotificationAgentDiscordModel    `tfsdk:"discord"`
-	Slack                 *NotificationAgentSlackModel      `tfsdk:"slack"`
-	Email                 *NotificationAgentEmailModel      `tfsdk:"email"`
-	LunaSea               *NotificationAgentLunaSeaModel    `tfsdk:"lunasea"`
-	Telegram              *NotificationAgentTelegramModel   `tfsdk:"telegram"`
-	Pushbullet            *NotificationAgentPushbulletModel `tfsdk:"pushbullet"`
-	Pushover              *NotificationAgentPushoverModel   `tfsdk:"pushover"`
-	Ntfy                  *NotificationAgentNtfyModel       `tfsdk:"ntfy"`
-	Webhook               *NotificationAgentWebhookModel    `tfsdk:"webhook"`
-	Gotify                *NotificationAgentGotifyModel     `tfsdk:"gotify"`
-	Webpush               *NotificationAgentWebpushModel    `tfsdk:"webpush"`
-	OnRequestPending      types.Bool                        `tfsdk:"on_request_pending"`
-	OnRequestApproved     types.Bool                        `tfsdk:"on_request_approved"`
-	OnRequestRejected     types.Bool                        `tfsdk:"on_request_rejected"`
-	OnRequestFailed       types.Bool                        `tfsdk:"on_request_failed"`
-	OnRequestAvailable    types.Bool                        `tfsdk:"on_request_available"`
-	OnRequestDeclined     types.Bool                        `tfsdk:"on_request_declined"`
-	OnRequestAutoApproved types.Bool                        `tfsdk:"on_request_auto_approved"`
-	OnMediaAvailable      types.Bool                        `tfsdk:"on_media_available"`
-	OnMediaFailed         types.Bool                        `tfsdk:"on_media_failed"`
-	OnMediaSkipped        types.Bool                        `tfsdk:"on_media_skipped"`
-	OnMediaIssued         types.Bool                        `tfsdk:"on_media_issued"`
-	OnMediaFollowed       types.Bool                        `tfsdk:"on_media_followed"`
-	OnIssueCreated        types.Bool                        `tfsdk:"on_issue_created"`
-	OnIssueComment        types.Bool                        `tfsdk:"on_issue_comment"`
-	OnIssueResolved       types.Bool                        `tfsdk:"on_issue_resolved"`
-	OnIssueReopened       types.Bool                        `tfsdk:"on_issue_reopened"`
-	OnMediaAutoRequested  types.Bool                        `tfsdk:"on_media_auto_requested"`
-}
-
 var _ datasource.DataSource = &NotificationClientDataSource{}
 
 func newNotificationClientDataSource(agent string) datasource.DataSource {
@@ -144,39 +109,5 @@ func (d *NotificationClientDataSource) Read(ctx context.Context, _ datasource.Re
 		resp.Diagnostics.AddError("Parse Failed", err.Error())
 		return
 	}
-
-	data := NotificationClientDataSourceModel{}
-	data.Enabled = resourceData.Enabled
-	data.EmbedPoster = resourceData.EmbedPoster
-	data.TypesMask = resourceData.TypesMask
-	data.Discord = resourceData.Discord
-	data.Slack = resourceData.Slack
-	data.Email = resourceData.Email
-	data.LunaSea = resourceData.LunaSea
-	data.Telegram = resourceData.Telegram
-	data.Pushbullet = resourceData.Pushbullet
-	data.Pushover = resourceData.Pushover
-	data.Ntfy = resourceData.Ntfy
-	data.Webhook = resourceData.Webhook
-	data.Gotify = resourceData.Gotify
-	data.Webpush = resourceData.Webpush
-	data.OnRequestPending = resourceData.OnRequestPending
-	data.OnRequestApproved = resourceData.OnRequestApproved
-	data.OnRequestRejected = resourceData.OnRequestRejected
-	data.OnRequestFailed = resourceData.OnRequestFailed
-	data.OnRequestAvailable = resourceData.OnRequestAvailable
-	data.OnRequestDeclined = resourceData.OnRequestDeclined
-	data.OnRequestAutoApproved = resourceData.OnRequestAutoApproved
-	data.OnMediaAvailable = resourceData.OnMediaAvailable
-	data.OnMediaFailed = resourceData.OnMediaFailed
-	data.OnMediaSkipped = resourceData.OnMediaSkipped
-	data.OnMediaIssued = resourceData.OnMediaIssued
-	data.OnMediaFollowed = resourceData.OnMediaFollowed
-	data.OnIssueCreated = resourceData.OnIssueCreated
-	data.OnIssueComment = resourceData.OnIssueComment
-	data.OnIssueResolved = resourceData.OnIssueResolved
-	data.OnIssueReopened = resourceData.OnIssueReopened
-	data.OnMediaAutoRequested = resourceData.OnMediaAutoRequested
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	resp.Diagnostics.Append(setNotificationClientState(ctx, &resp.State, &resourceData)...)
 }
